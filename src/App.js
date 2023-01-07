360e2739c75e309db76394991c6f7f70810efcbd
unknown <jeancarlosayala57@gmail.com>
2023-01-07 17:05:50 -0600

    config auth redirect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import { Route, Routes } from 'react-router-dom';
+import { useContext } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { Navigation } from './components/navigation';
+import { UserContext } from './context/user';
 import { Auth } from './pages/auth';
 import { Home } from './pages/home';
 import './styles/main.scss'
 
 function App() {
+
+  const { currentUser } = useContext(UserContext)
+
   return (
     <Routes>
       <Route path='/' element={<Navigation />} >
         <Route index element={<Home />} />
-        <Route path='auth' element={<Auth />} />
+        {
+          currentUser ?
+            <Route path='auth' element={<Navigate to='/' replace />} /> :
+            <Route path='auth' element={<Auth />} />
+        }
       </Route>
     </Routes>
   );
