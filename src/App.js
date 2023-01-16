1273ea7a223bea3138c5bafcc3d7898a02bf7f89
unknown <jeancarlosayala57@gmail.com>
2023-01-15 22:19:53 -0600

    new routes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { Navigation } from './components/navigation';
 import { UserContext } from './context/user';
 import { Auth } from './pages/auth';
+import { EditInfo } from './routes/editInfo';
 import { Home } from './pages/home';
 import './styles/main.scss'
+import { Panel } from './pages/panel';
+import { PanelNavigation } from './components/panelNavigation';
 
 function App() {
 
-  const { currentUser } = useContext(UserContext)
+  const { currentUser } = useContext(UserContext);
 
   return (
     <Routes>
@@ -16,10 +19,25 @@ function App() {
         <Route index element={<Home />} />
         {
           currentUser ?
-            <Route path='auth' element={<Navigate to='/' replace />} /> :
-            <Route path='auth' element={<Auth />} />
+            (
+              <>
+                <Route path='*' element={<Navigate to='/panel' replace />} />
+                <Route path='panel/:id' element={<EditInfo />} />
+              </>
+            ) :
+            (
+              <>
+                <Route path='auth' element={<Auth />} />
+                {/* <Route path='panel' element={<Navigate to='/auth' replace />} />
+                <Route path='panel/:id' element={<Navigate to='/auth' replace />} /> */}
+              </>
+            )
         }
       </Route>
+      <Route path='/panel' element={<PanelNavigation />}>
+        <Route index element={<Panel />} />
+      </Route>
+
     </Routes>
   );
 }
