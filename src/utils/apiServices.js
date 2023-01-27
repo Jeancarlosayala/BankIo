2912dbac4d773dd6aef9e5dae6c9057c43442053
unknown <jeancarlosayala57@gmail.com>
2023-01-27 13:29:48 -0600

    new api services

---
+++ src/utils/apiServices.js
@@ -0,0 +1,45 @@
+import spotify from '../images/services/spotify.png'
+import amazon from '../images/services/amazon.png'
+import crunchyroll from '../images/services/crunchyroll.png'
+import netflix from '../images/services/netflix.png'
+import rappi from '../images/services/rappi.png'
+import cinemex from '../images/services/cinemex.png'
+import att from '../images/services/at&t.png'
+
+export const apiService = [
+  {
+    'id': 1,
+    'name': 'Spotify',
+    'img': spotify
+  },
+  {
+    'id': 2,
+    'name': 'Amazon',
+    'img': amazon
+  },
+  {
+    'id': 3,
+    'name': 'Crunchyroll',
+    'img': crunchyroll
+  },
+  {
+    'id': 4,
+    'name': 'Netflix',
+    'img': netflix
+  },
+  {
+    'id': 5,
+    'name': 'Rappi',
+    'img': rappi
+  },
+  {
+    'id': 6,
+    'name': 'Cinemex',
+    'img': cinemex
+  },
+  {
+    'id': 7,
+    'name': 'AT&T',
+    'img': att
+  }
+]
