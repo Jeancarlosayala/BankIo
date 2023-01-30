a93f6d306524d364a7e03d29dbf853df664add73
Jeancarlosayala28 <carlos.ayala@puntocommerce.com>
2023-01-29 20:08:20 -0600

    using currentFomat and change comprobations

--- src/routes/panelTransfer.js
+++ src/routes/panelTransfer.js
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from 'react';
+import CurrencyFormat from 'react-currency-format';
 import { UserContext } from '../context/user';
-import { currencyFormat, uuid } from '../utils/creditCard'
+import { uuid } from '../utils/creditCard'
 import { executeTransfer } from '../utils/firebase';
 
 const creditCardInfo = {
@@ -8,17 +9,17 @@ const creditCardInfo = {
   quantityBalance: ''
 };
 
-export const TransferPanel = () => {
+export const PanelTransfer = () => {
   const { userInfo, userInfoTransfer, currentUser } = useContext(UserContext)
-  const [balance, setBalance] = useState(null);
+  const [balance, setBalance] = useState(0);
   const [loader, setLoader] = useState(false)
   const [formField, setFormField] = useState(creditCardInfo);
   const [userTransfer, setUserTransfer] = useState([])
 
 
   useEffect(() => {
-    setBalance(currencyFormat(+userInfo.balance))
-    if (balance === '$NaN' || balance === null) return setLoader(true);
+    setBalance(userInfo.balance)
+    if (balance === undefined || balance === null) return setLoader(true);
     setLoader(false)
   }, [balance, userInfo])
 
@@ -52,36 +53,35 @@ export const TransferPanel = () => {
       window.location.replace('/panel')
     }, 2000);
   }
+  if (loader) return <h1 className='text--bold'>Cargando...</h1>;
 
   return (
     <>
-      {
-        loader === false ? (
-          <>
-            <h1>{balance}</h1>
-            <form>
-              {
-                userTransfer.length > 0 && userTransfer[0].cardNumber !== userInfo.cardNumber ?
-                  (
-                    <>
-                      <form>
-                        <input onChange={handleChange}
-                          name='quantityBalance'
-                          placeholder='Cantidad a depositar'
-                          type='number' />
-                        <button onClick={transfer}>Transferir</button>
-                      </form>
-                    </>
-                  ) : userTransfer.length > 0 ?
-                    <h1>Porfavor utiliza un numero diferente al de tu tarjeta</h1> :
-                    <h1>No se ah encontrado un usuario</h1>
-              }
-              <input onChange={handleChange} name='cardNumber' placeholder='Numero de tarjeta' type='text' />
-              <button onClick={findUserTransfer}>Buscar</button>
-            </form>
-          </>
-        ) : <h1>Cargando...</h1>
-      }
+      <CurrencyFormat
+        value={balance}
+        displayType='text'
+        thousandSeparator={true}
+        prefix={'$'}
+        renderText={value => <span className='text--bold text--lg'>{value}</span>} />
+
+      <form>
+        {
+          userTransfer.length > 0 && userTransfer[0].cardNumber !== userInfo.cardNumber ?
+            (
+              <>
+                <input onChange={handleChange}
+                  name='quantityBalance'
+                  placeholder='Cantidad a depositar'
+                  type='number' />
+                <button onClick={transfer}>Transferir</button>
+              </>
+            ) : userTransfer.length > 0 ?
+              <h1>Porfavor utiliza un numero diferente al de tu tarjeta</h1> :
+              <h1>No se ah encontrado un usuario</h1>
+        }
+        <input onChange={handleChange} name='cardNumber' placeholder='Numero de tarjeta' type='text' />
+        <button onClick={findUserTransfer}>Buscar</button>
+      </form>
     </>
   )
 }
