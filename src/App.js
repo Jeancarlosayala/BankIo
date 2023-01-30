650816346dcddb9ffe5428d6c05f950b6e709940
Jeancarlosayala28 <carlos.ayala@puntocommerce.com>
2023-01-30 10:03:35 -0600

    loop condition in App fixed

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Outlet, Route, Routes, Navigate } from 'react-router-dom';
 import { Auth } from './pages/auth';
 import './styles/main.scss'
 import { Panel } from './pages/panel';
 import { PanelNavigation } from './components/panelNavigation';
 import { PanelTransfer } from './routes/panelTransfer';
 
-const islogged = localStorage.getItem('islogged')
-
 const PrivateRoute = () => {
-  if(!islogged) return <Navigate to='/auth' />
+  const isLogged = localStorage.getItem('islogged');
 
-  return (
-    <Routes>
-      <Route path='/' element={<PanelNavigation />}>
-        <Route index element={<Panel />} />
-        <Route path='service/:id' element={<h1>Services</h1>} />
-        <Route path='transaction' element={<PanelTransfer />} />
-      </Route>
-    </Routes>
-  )
+  if (!isLogged) return <Navigate to='/auth' />
+
+  return <Outlet />
 }
 
 function App() {
+
   return (
     <>
-      <PrivateRoute />
       <Routes>
+        <Route element={<PrivateRoute />}>
+          <Route path='/' element={<PanelNavigation />}>
+            <Route index element={<Panel />} />
+            <Route path='service/:id' element={<h1>Services</h1>} />
+            <Route path='transaction' element={<PanelTransfer />} />
+          </Route>
+        </Route>
         <Route path='auth' element={<Auth />} />
       </Routes>
     </>
